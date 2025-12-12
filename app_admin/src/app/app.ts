40eda23bc39165af28ed2ccaa799d6d8ff0b6c07
BLOXGATEOfficial <bloxgateofficial@gmail.com>
2025-12-12 18:16:37 -0500

    Module6 Checkpoint 2 - TripListing showing JSON data

--- app_admin/src/app/app.ts
+++ app_admin/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TripListing } from './trip-listing/trip-listing';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TripListing],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -0,0 +1,32 @@
+/*
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-listing',
+  imports: [],
+  templateUrl: './trip-listing.html',
+  styleUrl: './trip-listing.css',
+})
+export class TripListing {
+
+}*/
+
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trips } from '../data/trips';
+
+@Component({
+  selector: 'app-trip-listing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './trip-listing.html',
+  styleUrl: './trip-listing.css'
+})
+export class TripListing implements OnInit {
+  trips: Array<any> = trips;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
+
