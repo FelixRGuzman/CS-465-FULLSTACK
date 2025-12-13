838920da88ce138fb45f1ceab171362844706fba
BLOXGATEOfficial <bloxgateofficial@gmail.com>
2025-12-12 21:52:39 -0500

    Module6 Checkpoint 4 - Refactor trip rendering into TripCard component

--- app_admin/src/app/trip-listing/trip-listing.ts
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -14,14 +14,16 @@ export class TripListing {
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { trips } from '../data/trips';
+import { TripCard } from '../trip-card/trip-card';
 
 @Component({
   selector: 'app-trip-listing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TripCard],
   templateUrl: './trip-listing.html',
   styleUrl: './trip-listing.css'
 })
+
 export class TripListing implements OnInit {
   trips: Array<any> = trips;
 
