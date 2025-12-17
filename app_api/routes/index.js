5450a2515e1e5d3fa3ebd3bb2c0c9b9f463bfc39
Felix <FelixRGuzman@outlook.com>
2025-12-17 00:02:51 -0500

    CS 465 Final Project Submission

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken'); // Enable JSON Web Tokens
+
+
 const express = require("express");
 const router = express.Router();
 
@@ -6,12 +9,44 @@ const tripsController = require("../controllers/trips");
 
 const authController = require("../controllers/authentication");
 
-const authenticateJWT = (req, res, next) => {
-    // This is a temporary placeholder.
-    next(); 
-};
+// Method to authenticate our JWT
+function authenticateJWT(req, res, next) {
+// console.log('In Middleware');
+const authHeader = req.headers['authorization'];
+// console.log('Auth Header: ' + authHeader);
+if(authHeader == null)
+{
+console.log('Auth Header Required but NOT PRESENT!');
+return res.sendStatus(401);
+}
+let headers = authHeader.split(' ');
+if(headers.length < 1)
+{
+console.log('Not enough tokens in Auth Header: ' +
+headers.length);
+return res.sendStatus(501);
+}
+const token = authHeader.split(' ')[1];
+// console.log('Token: ' + token);
+if(token == null)
+{
+console.log('Null Bearer Token');
+return res.sendStatus(401);
+}
+// console.log(process.env.JWT_SECRET);
 
 // define route for our trips endpoint
+// console.log(jwt.decode(token));
+const verified = jwt.verify(token, process.env.JWT_SECRET, (err,
+verified) => {
+if(err)
+{
+return res.sendStatus(401).json('Token Validation Error!');
+}
+req.auth = verified; // Set the auth paramto the decoded object
+});
+next(); // We need to continue or this will hang forever
+}
 
 router.route("/register").post(authController.register);
 
@@ -34,6 +69,5 @@ router
     .route("/trips/:tripCode")
     .get(tripsController.tripsFindByCode) 
     .put(authenticateJWT,tripsController.tripsUpdateTrip);
-
 
 module.exports = router;
